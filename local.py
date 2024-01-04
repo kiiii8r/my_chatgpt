c66af31c17cddd4df654d58a93e90007f3b42635
kiiii8r <gkii.8r@gmail.com>
2024-01-04 17:30:04 +0900

    プラグイン分岐の追加など

--- local.py
+++ local.py
@@ -12,31 +12,35 @@ def main():
     init_page()
 
     # モデルの選択
-    llm = selet_model(config)
+    llm = select_model()
     
-    # メッセージの初期化
-    init_messages()
-
-    # ユーザーの入力を監視
-    if user_input := st.chat_input("聞きたいことを入力してね！"):
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        with st.spinner("ChatGPT が考えています ..."):
-            response = llm(st.session_state.messages)
-        st.session_state.messages.append(AIMessage(content=response.content))
-
+    # プラグインの選択
+    plugin = select_contents(llm)
     
-    # チャット履歴の表示
-    messages = st.session_state.get('messages', [])
-    for message in messages:
-        if isinstance(message, AIMessage):
-            with st.chat_message('assistant'):
-                st.markdown(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message('user'):
-                st.markdown(message.content)
-        else:  # isinstance(message, SystemMessage):
-            st.write(f"System message: {message.content}")
+    if plugin == "なし":
+        # メッセージの初期化
+        init_messages()
+    
+        # ユーザーの入力を監視
+        if user_input := st.chat_input("聞きたいことを入力してね！"):
+            st.session_state.messages.append(HumanMessage(content=user_input))
+            with st.spinner("ChatGPT が考えています ..."):
+                response = llm(st.session_state.messages)
+            st.session_state.messages.append(AIMessage(content=response.content))
+
         
+        # チャット履歴の表示
+        messages = st.session_state.get('messages', [])
+        for message in messages:
+            if isinstance(message, AIMessage):
+                with st.chat_message('assistant'):
+                    st.markdown(message.content)
+            elif isinstance(message, HumanMessage):
+                with st.chat_message('user'):
+                    st.markdown(message.content)
+            else:  # isinstance(message, SystemMessage):
+                st.write(f"System message: {message.content}")
+            
         
 def init_page():
     st.set_page_config(
@@ -56,9 +60,9 @@ def init_messages():
     st.session_state.cost = []
         
         
-def selet_model(config):
+def select_model():
     # サイドバーにモデル選択のラジオボタンを追加
-    model = st.sidebar.radio("Choose a model", ["GPT-3.5", "GPT-4"])
+    model = st.sidebar.radio("モデルを選択", ["GPT-3.5", "GPT-4"])
     if model == "GPT-3.5":
         model_name = "gpt-3.5-turbo-0613"
     else:
@@ -66,13 +70,27 @@ def selet_model(config):
         
     # サイドバーにスライダーを追加、temperatureの値を選択可能にする
     # 初期値は0.0、最小値は0.0、最大値は2.0、ステップは0.1
-    temperature = st.sidebar.slider("Temperature", 0.0, 2.0, 0.0, 0.1)
+    temperature = st.sidebar.slider("サンプリング温度", 0.0, 2.0, 0.0, 0.1)
     
     st.sidebar.markdown("## Costs")
     st.sidebar.markdown("**Total cost**")
     # st.sidebar.markdown(cb.total_cost)
 
     return ChatOpenAI(api_key=config['OPENAI_API_KEY'], temperature=temperature, model_name=model_name)        
+
+
+def select_contents(llm):
+    # サイドバーにモデル選択のラジオボタンを追加
+    plugin = st.sidebar.selectbox("プラグイン", ["なし", "WEBサイト要約", "Youtube動画要約", "PDF質問"])
+    if plugin == "WEBサイト要約":
+        plugin.web_summarize(llm)
+    elif plugin == "Youtube動画要約":
+        plugin.youtube_summarize(llm)
+    elif plugin == "PDF質問":
+        plugin.pdf_question(llm)
+    return plugin
+    
+    
 
 if __name__ == '__main__':
     main()
